8c724866a1eac3878e3fb03db87847c4068b10c4
Nicolas Deverge <ndeverge@ekito.fr>
2013-02-08 14:17:57 +0100

    Fin du coding dojo (à terminer pour les + courageux)

--- src/main/scala/StringCalculator.scala
+++ src/main/scala/StringCalculator.scala
@@ -1,5 +1,22 @@
 package org.nico.kata.stringcalculatorkata
 
 object StringCalculator {
-  
+
+
+
+	def calc (input:String) : Int = {
+		def isDelimiter(mayDelimiter: Char) : Boolean = mayDelimiter == ',' || mayDelimiter == '\n'
+
+		def calcIter(inputToConsume: Seq[Char], acc: Int) : Int = inputToConsume match {
+			case Nil => acc
+			case head::tail if (isDelimiter(head)) => { 
+				calcIter(tail, acc)
+			}
+			case head::tail => { 
+				calcIter(tail, head.toInt + acc)
+			}
+		}
+		calcIter(input.toSeq, 0)
+	}
 }
+
--- src/test/scala/StringCalculatorSpec.scala
+++ src/test/scala/StringCalculatorSpec.scala
@@ -15,4 +15,34 @@ class StringCalculatorSpec extends Specification {
       "Hello world" must endWith("world")
     }
   }
+  "Calc" should {
+    "return 0 when empty string" in {
+      StringCalculator.calc("") must beEqualTo(0)
+    }
+
+    "return 2 when string '2' passed" in {
+      StringCalculator.calc("2") must beEqualTo(2)
+    }
+
+    "return 3 when string '3' passed" in {
+      StringCalculator.calc("3") must beEqualTo(3)
+    }
+
+    "return 5 when string '2,3' passed" in {
+      StringCalculator.calc("2,3") must beEqualTo(5)
+    }
+
+    "return 4 when string '1,3' passed" in {
+      StringCalculator.calc("1,3") must beEqualTo(4)
+    }
+
+    "two numbers, newline delimited, returns the sum" in {
+      StringCalculator.calc("1\n3") must beEqualTo(4)
+    }
+  
+  } 
+
+
+
+
 }
