4cdb372b3c9e1fe61a482437f881faf45618b890
Nicolas Deverge <ndeverge@ekito.fr>
2013-03-04 14:18:34 +0100

    Fin du coding dojo

--- src/main/scala/StringCalculator.scala
+++ src/main/scala/StringCalculator.scala
@@ -1,5 +1,25 @@
 package org.nico.kata.stringcalculatorkata
 
 object StringCalculator {
+
+	def doSum(values: String): Int = {
+		var delimiter = 
+        if (values.startsWith("//")) {
+        	values.charAt(2)	
+        }	
+        else {
+        	""
+        }
+ 		val stringTokens = values.split(s"[,\n$delimiter]").filter(x => !x.startsWith("//"))
+		tabSum(stringTokens)
+	}
+
+	def tabSum(tab: Seq[String]): Int =  {
+		val tabOfNumbers: Seq[Int] = tab.map(x => x match {
+				case ""=> 0
+				case x => x.trim.toInt
+			})
+		tabOfNumbers.filter( _ < 1000).sum
+	} 
   
 }
--- src/test/scala/StringCalculatorSpec.scala
+++ src/test/scala/StringCalculatorSpec.scala
@@ -15,4 +15,42 @@ class StringCalculatorSpec extends Specification {
       "Hello world" must endWith("world")
     }
   }
+  "String Calculator" should {
+    "empty string returns zero" in {
+      StringCalculator.doSum("") must beEqualTo (0)
+    }
+    "A single number returns the value" in {
+      StringCalculator.doSum("12") must beEqualTo (12)
+    }
+
+    "two numbers, comma delimited, return the sum" in {
+      StringCalculator.doSum("12,12") must beEqualTo (24)
+    }
+
+    "Three numbers, comma delimited, return the sum" in {
+      StringCalculator.doSum("12,12,13") must beEqualTo (37)
+    }
+
+    "Two numbers, new line delimited, return the sum" in {
+      StringCalculator.doSum("1\n2") must beEqualTo (3) 
+    }
+
+    "Negative numbers should be handled" in {
+      StringCalculator.doSum("-1\n2") must beEqualTo (1)       
+    }
+
+    "Spaces should not be a problem" in {
+      StringCalculator.doSum("42, 8") must beEqualTo (50)       
+    }
+    "Big numbers should be ignored" in {
+      StringCalculator.doSum("1000, 1") must beEqualTo (1)       
+    }
+    "First line starting with // should be ignored" in {
+      StringCalculator.doSum("//t\n2") must beEqualTo (2)
+    }
+    "First line starting with // should define the delimiter" in {
+      StringCalculator.doSum("//#\n2#3") must beEqualTo (5)
+    }
+
+  }
 }
